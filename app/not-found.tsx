05a5e4bff60d317216f1868688cca8933f5164b4
thibault <thibault.Garrigues@icloud.Com>
2024-05-29 12:22:07 +0200

    Update styles on not-found.tsx to use Themes from theme provider

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,18 +4,18 @@ import { Button } from "@/components/ui/button"
  
 export default function NotFound() {
   return (
-    <div className="min-h-screen bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    <div className="min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
       <div className="mx-auto max-w-max">
         <main className="sm:flex">
-          <p className="text-4xl font-bold tracking-tight text-black sm:text-5xl">
+          <p className="text-4xl font-bold tracking-tight sm:text-5xl">
             404
           </p>
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+              <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
                 Page not found
               </h1>
-              <p className="mt-1 text-base text-gray-500">
+              <p className="mt-1 text-base">
                 Please check the URL in the address bar and try again.
               </p>
             </div>
