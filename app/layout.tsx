35b52bca243751a81a92f0d97fdc8008c9df3f52
thibault <thibault.Garrigues@icloud.Com>
2024-05-29 12:23:24 +0200

    fix text error on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
           <Toaster />
-        </ThemeProvider>Stashed changes
+        </ThemeProvider>
       </body>
     </SessionProvider>
     </html>
