60ca6ccf44862ea14fbb1c4b3c6810a5c69053a0
thibault <thibault.Garrigues@icloud.Com>
2024-05-29 16:44:30 +0200

    ADD HTTP request for login

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { signIn } from '@/auth';
+
+export async function POST(request: NextRequest): Promise<NextResponse> {
+  try {
+    const { email, password } = await request.json();
+    if (!email || !password) {
+      return NextResponse.json({ message: 'Email and password are required' }, { status: 400 });
+}
+    await signIn('credentials', {
+      redirect: false,
+      email,
+      password,
+    });
+    
+    return NextResponse.json({}, { status: 201 });
+  } catch (e: any) {
+    console.log(e);
+    return NextResponse.json({ message: e}, { status: 500 });
+  }
+}
