66eb1f7bc05f0d718cbb0273d8c853cd23a5732e
kennymnh <93839333+kennymnh@users.noreply.github.com>
2024-05-30 14:10:33 +0200

    Fix status code to be 200 instead of 201

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       password,
     });
     
-    return NextResponse.json({}, { status: 201 });
+    return NextResponse.json({}, { status: 200 });
   } catch (e: any) {
     console.log(e);
     return NextResponse.json({ message: e}, { status: 500 });
